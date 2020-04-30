daad609f2bd6a8a0de5ac7296ae464664ec43e4c
unknown <tecepeipe@hotmail.com>
2020-04-30 21:10:18 +1200

    another update

--- Plex-Export/generate.cmd
+++ Plex-Export/generate.cmd
@@ -8,13 +8,10 @@ php\php cli.php -sections="TV Series",Moviez,"Moviez 3D","Moviez Ultra HD",Anime
 
 
 ::delete and then upload data.js again to dropbox
-rclone sync --transfers 20 "C:\Users\fabricio\Documents\Home Page\Plex-Export\plex-data" dropbox-homepage:Apps/paperplane.io/tecepeipe.paperplane.io/plex-export/plex-data --progress --stats-one-line
 rclone sync --transfers 20 "C:\Users\fabricio\Documents\Home Page\Plex-Export\plex-data" onedrive-ubundle:"Home Page/Plex-Export/plex-data" --progress --stats-one-line
 rclone sync --transfers 20 "C:\Users\fabricio\Documents\Home Page\Plex-Export\plex-data" onedrive-mbundle1:"Home Page/Plex-Export/plex-data" --progress --stats-one-line
 rclone sync --transfers 20 "C:\Users\fabricio\Documents\Home Page\Plex-Export\plex-data" onedrive-mbundle2:"Home Page/Plex-Export/plex-data" --progress --stats-one-line
 rclone sync --transfers 20 "C:\Users\fabricio\Documents\Home Page\Plex-Export\plex-data" gdrive-bundle:"Home Page/Plex-Export/plex-data" --progress --stats-one-line
-rclone sync --transfers 1  "C:\Users\fabricio\Documents\Home Page\Plex-Export\plex-data" "E:/Home Page/Plex-Export/plex-data" --progress --stats-one-line
-
 
 
 cd "C:\Users\fabricio\Documents\Home Page"
