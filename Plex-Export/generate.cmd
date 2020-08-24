abc9808efd7f3d630bd51a8375f8adecc58ec033
unknown <tecepeipe@hotmail.com>
2020-08-25 11:04:04 +1200

    another plex export update

--- Plex-Export/generate.cmd
+++ Plex-Export/generate.cmd
@@ -8,10 +8,6 @@ php\php cli.php -sections="TV Series",Moviez,"Moviez 3D","Moviez Ultra HD",Anime
 
 
 rclone sync --transfers 20 "C:\Users\fabricio\Documents\Home Page\Plex-Export\plex-data" gdrive-bundle:"Home Page/Plex-Export/plex-data" --progress --stats-one-line
-rclone sync --transfers 20 "C:\Users\fabricio\Documents\Home Page\Plex-Export\plex-data" onedrive-ubundle:"Home Page/Plex-Export/plex-data" --progress --stats-one-line
-rclone sync --transfers 20 "C:\Users\fabricio\Documents\Home Page\Plex-Export\plex-data" onedrive-mbundle1:"Home Page/Plex-Export/plex-data" --progress --stats-one-line
-rclone sync --transfers 20 "C:\Users\fabricio\Documents\Home Page\Plex-Export\plex-data" onedrive-mbundle2:"Home Page/Plex-Export/plex-data" --progress --stats-one-line
-
 
 cd "C:\Users\fabricio\Documents\Home Page"
 git\bin\git add --all
