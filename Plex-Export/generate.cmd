a3aab6841d5f2fa10fc88a5bfc6cb33498684696
Fabricio <tecepeipe@hotmail.com>
2021-05-18 01:47:06 +1200

    another plex export update

--- Plex-Export/generate.cmd
+++ Plex-Export/generate.cmd
@@ -1,15 +1,15 @@
 @echo off
 c:
-cd "C:\Users\fabricio\Documents\Home Page\Plex-Export"
+cd "C:\Users\user\Documents\Home Page\Plex-Export"
 "C:\Program Files (x86)\Plex\Plex Media Server\Plex Media Server.exe"
 sleep 5s
 ::to get token, get media info, click View as XML and extract from URL
 php\php cli.php -sections="TV Series",Moviez,"Moviez 3D","Moviez Ultra HD",Animes,Cartoons -token=uPsjdxSwaJdMzrT6Uq2a
 
 
-rclone sync --transfers 20 "C:\Users\fabricio\Documents\Home Page\Plex-Export\plex-data" gdrive-bundle:"Home Page/Plex-Export/plex-data" --progress --stats-one-line
+rclone sync --transfers 20 "C:\Users\user\Documents\Home Page\Plex-Export\plex-data" gdrive-bundle:"Home Page/Plex-Export/plex-data" --progress --stats-one-line
 
-cd "C:\Users\fabricio\Documents\Home Page"
+cd "C:\Users\user\Documents\Home Page"
 git\bin\git add --all
 git\bin\git commit -m "another plex export update"
 git\bin\git push -u tecepeipe master
