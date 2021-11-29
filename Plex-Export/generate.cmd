bdacfc2ba8b575a85e950fabe99d5e9daf84dec6
Fabricio <tecepeipe@hotmail.com>
2021-11-29 15:18:40 +1300

    another update

--- Plex-Export/generate.cmd
+++ Plex-Export/generate.cmd
@@ -6,7 +6,7 @@ sleep 2s
 ::to get token, get media info, click View as XML and extract from URL
 php\php cli.php -sections="TV Series",Moviez,"Moviez 3D","Moviez Ultra HD",Animes,Cartoons -token=uPsjdxSwaJdMzrT6Uq2a
 
-
+rclone sync "C:\Users\User\Documents\Home Page" gdrive-bundle:"Home Page" -P --size-only --exclude git/** --exclude .git/**  --exclude Plex-Export/php/**
 rclone sync --transfers 20 "C:\Users\user\Documents\Home Page\Plex-Export\plex-data" gdrive-bundle:"Home Page/Plex-Export/plex-data" --progress --stats-one-line
 
 cd "C:\Users\user\Documents\Home Page"
