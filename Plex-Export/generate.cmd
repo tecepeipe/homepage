ea197ae6b9128f870aa283e031d8b8efef71ae67
Fabricio <tecepeipe@hotmail.com>
2022-03-20 20:57:11 +1300

    another plex export update

--- Plex-Export/generate.cmd
+++ Plex-Export/generate.cmd
@@ -4,7 +4,7 @@ cd "C:\Users\user\Documents\Home Page\Plex-Export"
 start /b "C:\Program Files (x86)\Plex\Plex Media Server\Plex Media Server.exe"
 sleep 2s
 ::to get token, get media info, click View as XML and extract from URL
-php\php cli.php -sections="TV Series",Moviez,"Moviez 3D","Moviez Ultra HD",Animes,Cartoons -token=uPsjdxSwaJdMzrT6Uq2a
+php\php cli.php -sections="TV Series",Moviez,"Moviez 3D","Moviez Ultra HD",Animes,Cartoons -token=_QfJSf7MLYHhonyZ2Uzs
 
 rclone sync "C:\Users\User\Documents\Home Page" gdrive-bundle:"Home Page" -P --size-only --exclude git/** --exclude .git/**  --exclude Plex-Export/php/**
 rclone sync --transfers 20 "C:\Users\user\Documents\Home Page\Plex-Export\plex-data" gdrive-bundle:"Home Page/Plex-Export/plex-data" --progress --stats-one-line
